effbfefe6e89d1ffabc40544a51f96a4e8691697
derek <derek@cogolabs.com>
2018-03-25 16:51:26 -0400

    refactor bayes for many distributions, add pareto

--- bayes.py
+++ bayes.py
@@ -9,49 +9,75 @@ import numpy as np
 import abtest
 
 
-class Variant:
+class Exponential:
 
-    def __init__(self, name, a, b, n, c, values=[]):
-        self.name = name
-        self.a = a
-        self.b = b
-        self.n = n
-        self.c = c
+    def __init__(self, values, frequencies, alpha=0, beta=0):
 
-        # this should have the form [[v1, v2...], [n1, n2...]] for
-        # all discrete values observed in the test
-        #
-        # always assuming an improper (0, 0) prior;
-        # make this more flexible later?
-        self.alpha = None
-        self.theta = None
-        if values:
-            self.alpha = sum(values[0])
-            self.theta = 1.0 / sum(
-                [i[0] * i[1] for i in zip(values[0], values[1])]
-            )
+        if len(values) != len(frequencies):
+            raise ValueError('values and frequencies must be of equal length')
+
+        self.alpha = alpha + sum(values)
+        self.theta = 1.0 / (
+            beta + sum([i[0] * i[1] for i in zip(values, frequencies)])
+        )
 
     def sample_posterior(self, n_samples):
-        self.samples = (np.random.beta(
-            self.a + self.c, self.b + self.n - self.c,
+        return np.random.gamma(
+            self.alpha,
+            scale=self.theta,
             size=n_samples
-        ))
-        if self.alpha and self.theta:
-            self.samples *= np.random.gamma(
-                self.alpha,
-                scale=self.theta,
-                size=n_samples
+        )
+
+
+class Pareto(Exponential):
+
+    def __init__(self, values, frequencies, alpha=0, beta=0, xmin=1):
+
+        if len(values) != len(frequencies):
+            raise ValueError('values and frequencies must be of equal length')
+
+        self.alpha = alpha + sum(values)
+        self.theta = 1.0 / (
+            beta + sum(
+                i[0] * np.log(i[1] / float(xmin))
+                for i in zip(values, frequencies)
+                if i[1] >= xmin
             )
+        )
+
+
+class Bernoulli:
+
+    def __init__(self, alpha, beta, trials, successes):
+        self.alpha = alpha + successes
+        self.beta = beta + trials - successes
+
+    def sample_posterior(self, n_samples):
+        return np.random.beta(
+            self.alpha,
+            self.beta,
+            size=n_samples
+        )
+
+
+class Variant:
+
+    def __init__(self, name, distributions):
+        self.name = name
+        self.distributions = distributions
+
+    def sample_posterior(self, n_samples):
+        self.samples = np.ones(n_samples)
+        for distribution in self.distributions:
+            self.samples *= distribution.sample_posterior(n_samples)
 
 
 class Test:
 
     def __init__(self, variants=None, **kwargs):
-        # note: variant[0] is taken to be control!
-        if variants:
-            self.variants = [Variant(*i) for i in variants]
-        else:
-            self.variants = []
+        # variants[0] is taken to be control!
+        self.variants = variants
+
         # 1M samples means loss function accurate to ~0.003 (p=(1 - 10^-10))
         self.n_samples = 1000000
         self.min_loss = 0.003
@@ -67,20 +93,20 @@ class Test:
             self.samples[i, :] = variant.samples
 
     def compute_statistics(self):
-        for i, variant in enumerate(self.variants):
+        for variant in self.variants:
 
             variant.beats_control = np.sum(
                 np.greater(
                     variant.samples,
                     self.variants[0].samples
                 )
-            ) / self.n_samples
+            ) / float(self.n_samples)
             variant.beats_all = np.sum(
                 np.equal(
                     variant.samples,
                     np.amax(self.samples, axis=0)
                 )
-            ) / self.n_samples
+            ) / float(self.n_samples)
             variant.loss = np.sum(
                 np.amax(
                     self.samples - np.tile(
@@ -88,7 +114,7 @@ class Test:
                     ),
                     axis=0
                 )
-            ) / self.n_samples
+            ) / float(self.n_samples)
 
     def run(self, abtest):
         self.abtest = abtest
@@ -148,9 +174,20 @@ class Test:
 
 
 def main():
+
     variants = [
-        ('control', 1, 1, 1281, 1281, [[1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 13, 20, 23], [1129, 67, 43, 16, 10, 1, 5, 2, 4, 1, 1, 1, 1]]),
-        ('treatment', 1, 1, 1236, 1236, [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 15, 18, 19], [1081, 66, 42, 16, 14, 2, 3, 4, 2, 2, 1, 1, 1, 1]]),
+        Variant(
+            'control',
+            [
+                Exponential([1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 13, 20, 23], [1129, 67, 43, 16, 10, 1, 5, 2, 4, 1, 1, 1, 1])
+            ]
+        ),
+        Variant(
+            'treatment',
+            [
+                Exponential([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 15, 18, 19], [1081, 66, 42, 16, 14, 2, 3, 4, 2, 2, 1, 1, 1, 1])
+            ]
+        )
     ]
     t = Test(variants=variants)
     t.report()
